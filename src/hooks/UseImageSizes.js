f776cc215c4714a87eaa77d64605dff3029ff9a1
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-06-30 15:48:52 -0400

    dev /30/2022

--- src/hooks/UseImageSizes.js
+++ src/hooks/UseImageSizes.js
@@ -1,33 +1,35 @@
-import { mapProps, parseSizeUnits, unwindProps, windProps } from 'dataweaver';
+import { mapProps, unwindProps, windProps, parseAndCalc } from 'dataweaver';
 import { useMemo } from 'react';
 
 const useImageSizes = (images, fallbackWidth, fallbackHeight) => {
-  const _images = useMemo(() => {
-    function _calc(parsedVars, fn) {
-      const varValues = parsedVars.map((v) => v && v.value);
-      const u = parsedVars.find(({ unit }) => unit).unit;
-      return `${fn(varValues)}${u}`;
-    }
-    function parseAndCalc(vars, fn) {
-      // const parsedVars = vars.map((v) => parseSizeUnits(v));
-      return _calc(parseSizeUnits(vars), fn);
-    }
-    return mapProps(
-      ({ images: imgs, fallbackWidth: width, fallbackHeight: height }) =>
-        imgs.map((img) => ({
-          width: parseAndCalc(
-            [width, img.colSpan],
-            ([_width, cSpan]) => _width * cSpan
-          ),
-          height: parseAndCalc(
-            [height, img.rowSpan],
-            ([_height, rSpan]) => _height * rSpan
-          ),
-          ...img,
-        })),
-      unwindProps({ images: [images], fallbackWidth, fallbackHeight })
-    );
-  }, [images, fallbackWidth, fallbackHeight]);
+  const _images = useMemo(
+    () =>
+      // function _calc(parsedVars, fn) {
+      //   const varValues = parsedVars.map((v) => v && v.value);
+      //   const u = parsedVars.find(({ unit }) => unit).unit;
+      //   return `${fn(varValues)}${u}`;
+      // }
+      // function parseAndCalc(vars, fn) {
+      //   // const parsedVars = vars.map((v) => parseSizeUnits(v));
+      //   return _calc(parseSizeUnits(vars), fn);
+      // }
+      mapProps(
+        ({ images: imgs, fallbackWidth: width, fallbackHeight: height }) =>
+          imgs.map((img) => ({
+            width: parseAndCalc(
+              [width, img.colSpan],
+              ([_width, cSpan]) => _width * cSpan
+            ),
+            height: parseAndCalc(
+              [height, img.rowSpan],
+              ([_height, rSpan]) => _height * rSpan
+            ),
+            ...img,
+          })),
+        unwindProps({ images: [images], fallbackWidth, fallbackHeight })
+      ),
+    [images, fallbackWidth, fallbackHeight]
+  );
 
   return windProps(_images);
 };
