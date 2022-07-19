e8161d3459e980f39fb82f249e470e2d5bb68790
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-07-19 14:43:06 -0400

    debug error when rowHeight is auto

--- src/hooks/UseImageSizes.js
+++ src/hooks/UseImageSizes.js
@@ -2,23 +2,25 @@ import { mapProps, unwindProps, windProps, parseAndCalc } from 'dataweaver';
 import { useMemo } from 'react';
 
 const useImageSizes = (images, fallbackWidth, fallbackHeight) => {
-  debugger;
   const _images = useMemo(
     () =>
       mapProps(
-        ({ images: imgs, width, height }) => ({
-          images: imgs.map((img) => ({
-            width: parseAndCalc(
-              [width, img.colSpan],
-              ([_width, cSpan]) => _width * cSpan
-            ),
-            height: parseAndCalc(
-              [height, img.rowSpan],
-              ([_height, rSpan]) => _height * rSpan
-            ),
-            ...img, // overwrite with provided width and height values
-          })),
-        }),
+        ({ images: imgs, width, height }) => {
+          debugger;
+          return {
+            images: imgs.map((img) => ({
+              width: parseAndCalc(
+                [width, img.colSpan],
+                ([_width, cSpan]) => _width * cSpan
+              ),
+              height: parseAndCalc(
+                [height, img.rowSpan],
+                ([_height, rSpan]) => _height * rSpan
+              ),
+              ...img, // overwrite with provided width and height values
+            })),
+          };
+        },
         unwindProps({
           images,
           width: fallbackWidth,
