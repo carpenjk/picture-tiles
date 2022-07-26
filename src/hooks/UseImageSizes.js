ab8b8b4f0509a36f462f010970b82859f1a46e1e
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-07-26 11:50:12 -0400

    removed dataweaver and themeweaver references/ replaced with prop-x

--- src/hooks/UseImageSizes.js
+++ src/hooks/UseImageSizes.js
@@ -2,29 +2,6 @@ import { unwindProps, windProps, parseAndCalc } from 'prop-x';
 import { useMemo } from 'react';
 
 const useImageSizes = (images, fallbackWidth, fallbackHeight) => {
-  // const _images = useMemo(
-  //   () =>
-  //     mapProps(
-  //       ({ images: imgs, width, height }) => ({
-  //         images: imgs.map((img) => ({
-  //           width: parseAndCalc(
-  //             [width, img.colSpan],
-  //             ([_width, cSpan]) => _width * cSpan
-  //           ),
-  //           height: parseAndCalc([height, img.rowSpan], ([_height, rSpan]) =>
-  //             _height && rSpan ? _height * rSpan : undefined
-  //           ),
-  //           ...img, // overwrite with provided width and height values
-  //         })),
-  //       }),
-  //       unwindProps({
-  //         images,
-  //         width: fallbackWidth,
-  //         height: fallbackHeight,
-  //       })
-  //     ),
-  //   [images, fallbackWidth, fallbackHeight]
-  // );
   const _images = useMemo(
     () =>
       unwindProps({
