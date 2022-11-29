d6e166009125bec94676717ed86c21d3a5fe3815
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-11-29 15:41:47 -0500

    fix(useimagesizes.js): updated UseImagesSizes.js for new images approach

--- src/hooks/UseImageSizes.js
+++ src/hooks/UseImageSizes.js
@@ -1,28 +1,17 @@
-import { unwindProps, windProps, parseAndCalc } from '@carpenjk/prop-x';
 import { useMemo } from 'react';
 
 const useImageSizes = (images, fallbackWidth, fallbackHeight) => {
   const _images = useMemo(
     () =>
-      unwindProps({
-        images: [images],
+      images.map((img) => ({
         width: fallbackWidth,
         height: fallbackHeight,
-      }).map(({ images: imgs, width, height }) => ({
-        images: imgs.map((img) => ({
-          width: parseAndCalc(
-            [width, img.colSpan],
-            ([_width, cSpan]) => _width * cSpan
-          ),
-          height: parseAndCalc([height, img.rowSpan], ([_height, rSpan]) =>
-            _height && rSpan ? _height * rSpan : undefined
-          ),
-          ...img, // overwrite with provided width and height values
-        })),
+        ...img,
       })),
     [images, fallbackWidth, fallbackHeight]
   );
-  return windProps(_images).images;
+
+  return _images;
 };
 
 export default useImageSizes;
