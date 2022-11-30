28b7b6581a313afcb325c61111cfb011fff81061
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-11-30 12:54:00 -0500

    fix(imgbutton.jsx): update ImgButton display property for new hide images approach

--- src/components/ImgButton.jsx
+++ src/components/ImgButton.jsx
@@ -1,13 +1,14 @@
 import StyledImgButton from './styled/StyledImgButton';
 
-const ImgButton = ({ onClick, type, hide, alt, ...imgProps }) => (
-  <StyledImgButton
-    onClick={onClick}
-    type={type}
-    display={hide ? 'none' : 'inline-block'}
-  >
-    <img alt={alt} {...imgProps} />
-  </StyledImgButton>
-);
+const ImgButton = ({ onClick, type, hide, alt, ...imgProps }) => {
+  const hideAry = Array.isArray(hide) ? hide : [hide];
+  const display = hideAry.map((h) => (h ? 'none' : 'inline-block'));
+
+  return (
+    <StyledImgButton onClick={onClick} type={type} display={display}>
+      <img alt={alt} {...imgProps} />
+    </StyledImgButton>
+  );
+};
 
 export default ImgButton;
