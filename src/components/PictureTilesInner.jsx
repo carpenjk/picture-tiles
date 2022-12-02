472f5e9daa07f348addd18af8489229ccd9af207
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-12-02 13:30:30 -0500

    fix(picturetilesinner.jsx): updated Tile key so Tiles don't remount at each breakpoint

--- src/components/PictureTilesInner.jsx
+++ src/components/PictureTilesInner.jsx
@@ -1,7 +1,3 @@
-import { ThemeContext } from 'styled-components';
-import { useContext } from 'react';
-import { useBreakpoints } from '@carpenjk/prop-x/useBreakpoints';
-import { getPropIndex } from '@carpenjk/prop-x';
 import { useImageLoader } from './imageLoader/ImageLoader';
 import ImageWrapper from './imageLoader/ImageWrapper';
 import ImgButton from './ImgButton';
@@ -9,19 +5,12 @@ import Tile from './Tile';
 
 const PictureTilesInner = ({ images, onPhotoClick }) => {
   const { onLoad } = useImageLoader();
-  const theme = useContext(ThemeContext);
-  const br = useBreakpoints(theme.breakpoints);
-  const currBrIndex = getPropIndex(images, br.indexOfLower);
   return (
     <>
       {images.map((img, i) => {
         const { rowSpan, colSpan, ...imgProps } = img;
         return (
-          <Tile
-            key={`${currBrIndex}${i}`}
-            rowSpan={img.rowSpan}
-            colSpan={img.colSpan}
-          >
+          <Tile key={i} rowSpan={img.rowSpan} colSpan={img.colSpan}>
             <ImageWrapper id={i}>
               <ImgButton
                 type="button"
